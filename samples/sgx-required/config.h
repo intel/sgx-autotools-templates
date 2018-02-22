276e0ffe90a3a6fc0fabce9b223af3912432c109
Mechalas, John P <john.p.mechalas@intel.com>
2018-02-22 15:05:55 -0800

    Adapt makefiles. Create second sample.

---
+++ samples/sgx-required/config.h
@@ -0,0 +1,41 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Enable debugging */
+#define DEBUG 1
+
+/* Flag set for prerelease builds */
+/* #undef EDEBUG */
+
+/* Flag set for prerelease and release builds */
+/* #undef NDEBUG */
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "sgx-autotools"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "john.p.mechalas@intel.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "sgx-autotools"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "sgx-autotools 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "sgx-autotools"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Enable hardware simulation mode */
+/* #undef SGX_HW_SIM */
+
+/* Version number of package */
+#define VERSION "1.0"
